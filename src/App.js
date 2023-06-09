9c234e3c4690c0275812c704b485f87f6dac8250
AVA-Sneha <127439304+AVA-Sneha@users.noreply.github.com>
2023-06-09 22:16:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function login(e) {
     email: document.getElementById('exampleInputEmail1').value,
     password: document.getElementById('exampleInputPassword1').value
   }
-  axios.post('http://20.72.176.228:8080/login', request)
+  axios.post('http://20.246.170.139:9000', request)
   .then( resp => {
     alert(resp.data.message);
   })
